722768d7ed4ed678525172db3c65a815edc71867
unknown <dilshodadilbekov456@gmail.com>
2025-01-02 11:52:31 +0600

    hello

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section>
+      <div className='container'>
+        <div>
+          Home
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
